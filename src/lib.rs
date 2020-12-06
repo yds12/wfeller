f1b3afe1633a87da2a5a844a3365e6cd48cc3227
yds12 <28710965+yds12@users.noreply.github.com>
2020-12-06 19:48:24 +0100

    First struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,32 @@
+
+/// A distribution of n distinct elements in m possible cells
+pub struct Distribution {
+  n: u16,
+  m: u16
+}
+
+impl Distribution {
+  pub fn new(n: u16, m: u16) -> Self {
+    Distribution {
+      n: n,
+      m: m
+    }
+  }
+
+  pub fn how_many(self: Self) -> u32 {
+    return 0;
+  }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+  use super::*;
+
+  #[test]
+  fn new() {
+    let distr = Distribution::new(1, 2);
+    assert_eq!(distr.n, 1);
+    assert_eq!(distr.m, 2);
+  }
 }
+
