674efdccc8a011aaa4dd822bbbedfffe65ebed9c
yds12 <28710965+yds12@users.noreply.github.com>
2020-12-06 19:57:10 +0100

    Calculates number of combinations.

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,8 @@ impl Distribution {
     }
   }
 
-  pub fn how_many(self: Self) -> u32 {
-    return 0;
+  pub fn how_many(self: Self) -> u64 {
+    return (self.n as u64).pow(self.m as u32);
   }
 }
 
@@ -27,6 +27,21 @@ mod tests {
     let distr = Distribution::new(1, 2);
     assert_eq!(distr.n, 1);
     assert_eq!(distr.m, 2);
+  }
+
+  #[test]
+  fn how_many() {
+    let distr = Distribution::new(1, 1);
+    assert_eq!(distr.how_many(), 1);
+
+    let distr = Distribution::new(3, 3);
+    assert_eq!(distr.how_many(), 27);
+
+    let distr = Distribution::new(4, 3);
+    assert_eq!(distr.how_many(), 64);
+
+    let distr = Distribution::new(10, 10);
+    assert_eq!(distr.how_many(), 10_000_000_000);
   }
 }
 
