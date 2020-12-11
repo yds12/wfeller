64e4084217ef9ebcfb4ffcfa6d402d4502e08b99
yds12 <28710965+yds12@users.noreply.github.com>
2020-12-11 22:51:09 +0100

    Iterator compiles. Now needs to generate correct items.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,49 @@
 
 /// A distribution of n distinct elements in m possible cells
 pub struct Distribution {
-  n: u16,
-  m: u16
+  pub n: u16,
+  pub m: u16
+}
+
+pub struct DistributionIter<'a> {
+  distr: &'a Distribution,
+  index: u64
+}
+
+impl<'a> IntoIterator for &'a Distribution {
+  type Item = Vec<u16>;
+  type IntoIter = DistributionIter<'a>;
+
+  fn into_iter(self) -> Self::IntoIter {
+    DistributionIter {
+      distr: &self,
+      index: 0
+    }
+  }
+}
+
+impl<'a> Iterator for DistributionIter<'a> {
+  type Item = Vec<u16>;
+
+  fn next(self: &mut Self) -> Option<Self::Item> {
+    if self.index < self.distr.how_many() {
+      self.index += 1;
+      Some(vec![0; self.distr.m as usize])
+    } else {
+      None
+    }
+  }
 }
 
 impl Distribution {
   pub fn new(n: u16, m: u16) -> Self {
     Distribution {
-      n: n,
-      m: m
+      n,
+      m
     }
   }
 
-  pub fn how_many(self: Self) -> u64 {
+  pub fn how_many(self: &Self) -> u64 {
     return (self.n as u64).pow(self.m as u32);
   }
 }
@@ -42,6 +72,18 @@ mod tests {
 
     let distr = Distribution::new(10, 10);
     assert_eq!(distr.how_many(), 10_000_000_000);
+  }
+
+  #[test]
+  fn iter() {
+    let distr = Distribution::new(3, 3);
+
+    let mut x = 0;
+    for dist_vec in &distr {
+      println!("{:?}", dist_vec);
+      x += 1;
+    }
+    assert_eq!(x, 27);
   }
 }
 
