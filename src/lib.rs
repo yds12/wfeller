bc3398895d6196ceeb2ed474b69bfd71fb07a08f
yds12 <28710965+yds12@users.noreply.github.com>
2021-01-01 19:45:54 +0100

    Correctly generating distributions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-
 /// A distribution of n distinct elements in m possible cells
+#[derive(Debug)]
 pub struct Distribution {
   pub n: u16,
   pub m: u16
@@ -27,8 +27,15 @@ impl<'a> Iterator for DistributionIter<'a> {
 
   fn next(self: &mut Self) -> Option<Self::Item> {
     if self.index < self.distr.how_many() {
+      let mut distr = vec![0; self.distr.n as usize];
+
+      for i in 0..self.distr.n {
+        distr[i as usize] = ((self.index / (self.distr.m.pow(i as u32) as u64))
+          % (self.distr.m as u64)) as u16;
+      }
+
       self.index += 1;
-      Some(vec![0; self.distr.m as usize])
+      Some(distr)
     } else {
       None
     }
@@ -44,7 +51,7 @@ impl Distribution {
   }
 
   pub fn how_many(self: &Self) -> u64 {
-    return (self.n as u64).pow(self.m as u32);
+    return (self.m as u64).pow(self.n as u32);
   }
 }
 
@@ -64,11 +71,17 @@ mod tests {
     let distr = Distribution::new(1, 1);
     assert_eq!(distr.how_many(), 1);
 
+    let distr = Distribution::new(2, 3);
+    assert_eq!(distr.how_many(), 9);
+
+    let distr = Distribution::new(3, 2);
+    assert_eq!(distr.how_many(), 8);
+
     let distr = Distribution::new(3, 3);
     assert_eq!(distr.how_many(), 27);
 
     let distr = Distribution::new(4, 3);
-    assert_eq!(distr.how_many(), 64);
+    assert_eq!(distr.how_many(), 81);
 
     let distr = Distribution::new(10, 10);
     assert_eq!(distr.how_many(), 10_000_000_000);
@@ -77,6 +90,7 @@ mod tests {
   #[test]
   fn iter() {
     let distr = Distribution::new(3, 3);
+    println!("{:?}", distr);
 
     let mut x = 0;
     for dist_vec in &distr {
@@ -84,6 +98,26 @@ mod tests {
       x += 1;
     }
     assert_eq!(x, 27);
+
+    let distr = Distribution::new(2, 3);
+    println!("{:?}", distr);
+
+    let mut x = 0;
+    for dist_vec in &distr {
+      println!("{:?}", dist_vec);
+      x += 1;
+    }
+    assert_eq!(x, 9);
+
+    let distr = Distribution::new(3, 2);
+    println!("{:?}", distr);
+
+    let mut x = 0;
+    for dist_vec in &distr {
+      println!("{:?}", dist_vec);
+      x += 1;
+    }
+    assert_eq!(x, 8);
   }
 }
 
